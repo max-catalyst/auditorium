fe32390e6782c8c90749ee4c4772440242c14373
Kili S <79143155+klinshy@users.noreply.github.com>
2024-09-13 11:24:04 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
 
-console.log('Script started successfully');
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
 
 let currentPopup: any = undefined;
 
